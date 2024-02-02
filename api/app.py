70c38ad03e1e739979526cfebba4d9b2bae1d4dd
Jordy <jordycorporan95@gmail.com>
2024-02-02 14:57:53 -0500

    praying it works

--- api/app.py
+++ api/app.py
@@ -1,17 +1,25 @@
+import os
 from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf.csrf import generate_csrf
 from models import db
-from config import Config
+from config import ProductionConfig, Config
 from routes import bookings_routes, user_routes, auth_routes, home_page_routes,about_page_routes,contact_page_routes,faq_page_routes,service_page_routes,testimonial_page_routes,resource_page_routes
 from seeds import seed_commands
 
 app = Flask(__name__)
+
+if os.getenv('FLASK_ENV') == 'production':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(Config)
+
 app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.unauthorized'
 # register blue prints here
--- api/config.py
+++ api/config.py
@@ -1,13 +1,15 @@
 import os
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
-    # (only 'postgresql') but heroku's postgres add-on automatically sets the
-    # url in the hidden config vars to start with postgres.
-    # so the connection uri must be updated here (for production)
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
-    SQLALCHEMY_ECHO = True
+    # Default to using SQLite for development
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL')
+    if SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+    else:
+        # Fall back to the development URI or some default
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'  # or any other fallback
