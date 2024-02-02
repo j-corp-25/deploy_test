9574a0a5ac576e8574328aa57419ddf4e8a9a196
Jordy <jordycorporan95@gmail.com>
2024-02-02 03:55:42 -0500

    connect backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5000/api/:path*'
-            : '/api/',
+            ? 'http://127.0.0.1:5000/api/:path*' // Local Flask server
+            : undefined, // Use the same path in production
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
