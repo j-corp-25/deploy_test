2a8f4ef8146017d681cd3758483f3e15b7fcd3f4
Jordy <jordycorporan95@gmail.com>
2024-02-02 04:02:29 -0500

    try again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
+  rewrites: async () => {
     return [
       {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5000/api/:path*' // Local Flask server
-            : undefined, // Use the same path in production
+            ? 'http://127.0.0.1:5328/api/:path*'
+            : '/api/',
       },
-    ];
+    ]
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
