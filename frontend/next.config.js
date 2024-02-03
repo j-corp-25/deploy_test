e2791b58f9b76754ae8037028154e26a8348b7a2
Jordy <jordycorporan95@gmail.com>
2024-02-02 20:39:57 -0500

    ga

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '../api/:path*',
+        source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:5000/api/:path*'
