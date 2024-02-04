87c385cdd458e9628fe7b818b9922a20427571fb
Jordy <jordycorporan95@gmail.com>
2024-02-04 01:40:26 -0500

    pray

--- api/config.py
+++ api/config.py
@@ -3,13 +3,15 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Default to using SQLite for development
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+    FLASK_APP = os.getenv('FLASK_APP')
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL')
-    if SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
-    else:
-        # Fall back to the development URI or some default
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'  # or any other fallback
+    # In production, use the POSTGRES_URL environment variable provided by Vercel
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL').replace('postgres://', 'postgresql://')
+
+# Determine which config to load
+if os.getenv('VERCEL_ENV') == 'production':
+    current_config = ProductionConfig()
+else:
+    current_config = Config()
